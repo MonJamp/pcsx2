721e649e92d2951c1fe6dc214f96e6611cced30e
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2020-09-04 08:36:09 +0200

    CDVD: polishing rebase and fixing nullptr bug

--- pcsx2/gui/AppMain.cpp
+++ pcsx2/gui/AppMain.cpp
@@ -1177,6 +1177,7 @@ void SysStatus( const wxString& text )
 void SysUpdateIsoSrcFile( const wxString& newIsoFile )
 {
 	g_Conf->CurrentIso = newIsoFile;
+	sMainFrame.UpdateStatusBar();
 	sMainFrame.UpdateCdvdSrcSelection();
 }
 
